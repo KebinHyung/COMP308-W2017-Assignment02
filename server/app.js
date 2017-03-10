de6f4c195dcab2e9c849cee2d357600135d1f5a9
Kevin Luu <kevin_luu_naruto@hotmail.com>
2017-03-10 03:30:57 -0500

    First Commit

---
+++ server/app.js
@@ -0,0 +1,89 @@
+/*
+Kevin Luu 
+Feb. 25, 2017
+Added Functionality: Initalize Session and use flash
+*/
+
+// modules required for the project
+let express = require('express');
+let path = require('path'); // part of node.js core
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+
+// import "mongoose" - required for DB Access
+let mongoose = require('mongoose');
+// URI
+let config = require('./config/db');
+
+//modules installed for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportlocal = require('passport-local');
+let LocalStrategy = passportlocal.Strategy;
+let flash = require('connect-flash'); // displays errors / login messages
+
+mongoose.connect(process.env.URI || config.URI);
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("Conneced to MongoDB...");
+});
+
+// define routers
+let index = require('./routes/index'); // top level routes
+let books = require('./routes/books'); // routes for books
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /client
+app.use(favicon(path.join(__dirname, '../client', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client')));
+
+// setup session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: true,
+  resave: true
+}));
+
+// initialize passport and flash
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// route redirects
+app.use('/', index);
+app.use('/books', books);
+
+
+
+// Handle 404 Errors
+  app.use(function(req, res) {
+      res.status(400);
+     res.render('errors/404',{
+      title: '404: File Not Found'
+    });
+  });
+
+  // Handle 500 Errors
+  app.use(function(error, req, res, next) {
+      res.status(500);
+      res.render('errors/500', {
+        title:'500: Internal Server Error',
+        error: error
+      });
+  });
+
+module.exports = app;
